e5583f0c80aa954abd995b220dc499f3ec352bc7
GUPPi <110960330+guppi21@users.noreply.github.com>
2026-05-27 13:25:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import axios from "axios";
 
-const CONTRACT_ADDRESS = "0x0FdF4894a3b7C5a101686829063BE52Ad45bcfb7";
+const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
 
 const ABI = [
   "function mintCapsule(string memory metadataURI,uint256 unlockDate) public",
