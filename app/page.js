ff42521b30651a8ca6009c26cc4bc7abf9ad92a7
GUPPi <110960330+guppi21@users.noreply.github.com>
2026-05-27 11:37:44 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -86,7 +86,7 @@ export default function Home() {
       metadata,
       {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
+          Authorization: `Bearer ${process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI3MWVjODQ5YS0yNTEwLTRkY2QtOWViNC04MjE1ZTIxZjQwMDIiLCJlbWFpbCI6InNhYXJ0aGFra2FkYW0yMUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiMWU5MmVjMGQ1ZGMyNDc5YWJmZmMiLCJzY29wZWRLZXlTZWNyZXQiOiIxYmQ1NzFlMzU1ODA4ODcxZjI2ODNmODdiMmM4NWYwNTZjOTcxMWVlMGMzZmZmMDA1ODNkY2I0YzdiNDljNTc4IiwiZXhwIjoxODExMzk4MDAyfQ.QW5G3D-LgsGn6KDqFpprFYWW8yboZFcz9MOjUvGN2Uk}`,
         },
       }
     );
